81c5598a1a6e84b66b30dfecdfde057b9ecaf2c4
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2022-08-18 18:18:19 +0900

    Changed to automatically create the log directory if it does not exist.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,8 +1,14 @@
+import os
 import logging
 import datetime
 
 # Log file name. It is the current date.
 now = datetime.datetime.now().strftime('%Y%m%d')
+
+# If there is no log output destination, create one.
+logDir = 'logs'
+if not os.path.exists(logDir):
+  os.makedirs(logDir)
 
 # Initialize the logger.
 logging.basicConfig(
@@ -10,7 +16,7 @@ logging.basicConfig(
   format = '%(levelname)s - %(asctime)s -> %(filename)s(%(lineno)s): %(message)s',
   encoding = 'utf-8',
   handlers=[
-    logging.FileHandler(f'logs/{now}.log')
+    logging.FileHandler(f'{logDir}/{now}.log')
     # Output to console window.
     # ,logging.StreamHandler()
   ]
